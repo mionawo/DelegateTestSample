ca327909453e8ca47d5f35f08ac9411cb0e989c7
misato <misato@narusemisato-no-MacBook.local>
2016-12-23 13:46:51 +0900

    delegateなしverのfirstcommit

--- delegatetest/ViewController.swift
+++ delegatetest/ViewController.swift
@@ -8,17 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController, testDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    @IBOutlet weak var delegateTableView: DelegateTableView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        delegateTableView.testdelegate = self
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        let nib = UINib(nibName: "DelegateTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "DelegateTableViewCell")
+
+    }
+
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DelegateTableViewCell")!
+        
+        return cell
     }
     
-    func test() {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
         guard let url = URL(string: "https://www.youtube.com/watch?v=GNIkcjccZlw") else {
             return
         }
@@ -28,4 +52,5 @@ class ViewController: UIViewController, testDelegate {
         }
     }
 }
+
 
